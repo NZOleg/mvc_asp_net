bf1b4ea7477834a99f7e8dbf52690cd99d559ae3
Oleg Nesterov <nzolegok@gmail.com>
2018-03-20 10:43:29 +1300

    init

---
+++ Store/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderRepository repository;
+        private Cart cart;
+        public OrderController(IOrderRepository repoService, Cart cartService)
+        {
+            repository = repoService;
+            cart = cartService;
+        }
+        public ViewResult List() =>
+View(repository.Orders.Where(o => !o.Shipped));
+        [HttpPost]
+        public IActionResult MarkShipped(int orderID)
+        {
+            Models.Order order = repository.Orders
+            .FirstOrDefault(o => o.OrderID == orderID);
+            if (order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
+
+        public ViewResult Checkout() => View(new Models.Order());
+        [HttpPost]
+        public IActionResult Checkout(Models.Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
+        }
+    }
+}
---
+++ Store/Models/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Store.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+
+    public class ApplicationDbContextFactory
+            : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+
+        public ApplicationDbContext CreateDbContext(string[] args) =>
+            Program.BuildWebHost(args).Services
+                .GetRequiredService<ApplicationDbContext>();
+    }
+}
